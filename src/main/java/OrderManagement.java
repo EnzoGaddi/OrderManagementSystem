9d6837e40f53b7e245ba89da7847f66921b67521
ENZOGADDI-STUDENT <G00431922@atu.ie>
2024-11-04 10:40:34 +0000

    added UI

---
+++ src/main/java/OrderManagement.java
@@ -0,0 +1,37 @@
+
+import java.util.Scanner;
+import java.util.ArrayList;
+public class OrderManagement {
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        double List = list.nextDouble;
+
+        ArrayList<String, String, int> newOrder = new ArrayList<>();
+
+        System.out.println("Add Order\n");
+        String addOrder = sc.nextLine();
+
+        System.out.println("Update Order\n");
+        String updateOrder = sc.nextLine();
+
+        System.out.println("Display All Orders\n");
+        String displayOrder = sc.nextLine();
+
+        System.out.println("Exit\n");
+
+      switch (sc) {
+          case -> '1':
+          System.out.println("Enter customer name: ");
+          String name = sc.nextLine();
+
+          System.out.println("Enter product: ");
+          String product = sc.nextLine();
+
+          System.out.println("Enter amount: ");
+          int amount =
+
+      }
+
+    }
+}
